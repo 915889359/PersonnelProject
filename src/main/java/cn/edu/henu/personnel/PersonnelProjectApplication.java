f637624d911d225b1b07afe57efa7105e69dfdd4
yang <35619144+915889359@users.noreply.github.com>
2019-04-18 20:46:22 +0800

    Add files via upload
    项目整体框架搭建，为使项目能够运行，暂时没有连接数据库

---
+++ src/main/java/cn/edu/henu/personnel/PersonnelProjectApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.henu.personnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonnelProjectApplication.class, args);
+    }
+
+}
